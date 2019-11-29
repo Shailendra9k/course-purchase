fc3c47bb5259ad8d37a31eb2027904456f1ebdc8
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-29 23:12:00 +0200

    coursesales component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Coursesales from "./components/Coursesales";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let courses = [
+      { name: "Complete FullStack dev course", price: 199 },
+      { name: "Complete Pentesting course", price: 299 },
+      { name: "Complete Frontend dev course", price: 180 },
+      { name: "Bug Bounty and web app pentesting", price: 190 }
+    ];
+    return (
+      <div className="App">
+        <div>
+          <p>Welcome to course sales page.</p>
+          <Coursesales items={courses} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Coursesales.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Coursesales extends Component {
+  render() {
+    console.log(this.props.items);
+    return <div>I am from course sales page.</div>;
+  }
+}
+
+export default Coursesales;
